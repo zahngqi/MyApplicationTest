5376c5fb356fc08d0bf0168282e58e6579af5028
zhangqi <zhangqi@mobiwire.com.hk>
2017-10-12 17:38:29 +0800

    zhehsi yige ce shi

--- app/src/main/java/com/example/zhangqi/myapplication/MainActivity.java
+++ app/src/main/java/com/example/zhangqi/myapplication/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.zhangqi.myapplication;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -12,6 +14,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-
+        final TextView textView =(TextView) findViewById(R.id.text);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(textView.getText().equals("Hello git")){
+                    textView.setText("HELLO GIT");
+                    textView.setTextColor(145524);
+                }else
+                    textView.setTextColor(000000);
+            }
+        });
     }
 }
